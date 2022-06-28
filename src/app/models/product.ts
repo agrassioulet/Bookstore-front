23b2fda810592ca1a14d87fca18afa11a3954457
Alexandre <alexandre.grassioulet@hotmail.fr>
2022-06-28 12:45:46 +0200

    on product-list

---
+++ src/app/models/product.ts
@@ -0,0 +1,14 @@
+export interface IProduct {
+    title:  string;
+    description:  string;
+    price: number;
+    brand:  string;
+    category:  string;
+    img_url:  string;
+}
+
+
+export class ProductOperators {
+
+    
+}
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { IProduct } from '../models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  url = environment.URL_BACKEND + '/product'
+  constructor(private httpClient: HttpClient) {
+  }
+
+  public getAllProducts() {
+    return this.httpClient.get<IProduct[]>(this.url + '/get-all-products');
+  }
+
+  public getProductById(id: string): Observable<any> {
+    return this.httpClient.get<any>(this.url + '/get-product-by-id' + '/' + id);
+  }
+
+  public getProductsByCategory(category: string): Observable<any> {
+    return this.httpClient.get<any>(this.url + '/get-products-by-category' + '/' + category);
+  }
+
+  public createProduct(product: any): Observable<void> {
+    return this.httpClient.post<void>(this.url, product)
+  }
+
 }
